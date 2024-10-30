b192e81dc5e534ea0b12a784b2b4b039c8cabeef
graja <lalumaurindra@gmail.com>
2024-10-30 10:54:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function performSearch() {
     const guides = [
         { title: 'Mati Total / Matot', content: '1. Cek fisik motherboard terlebih dahulu, pastikan tidak ada komponen yang hilang atau korosi, jika ada lengkapi dan bersihkan.<br>2. Jika sudah aman, selanjutnya cek buzzer pada setiap PL laptop, pastikan tidak ada PL Buzzer kecuali PL CPU,VGA.<br>3. Jika buzzer aman, maka boleh mencolokan tegangan ke motherboard, cek tegangan R Adsen (19v), jika tidak ada urut kebelakang apakah ada part bermasalah,walau r adsen keluar namun led tak menyala pastikan lagi pada bagian ic charge dan sekitarnya terutama resistor dan mosfet.<br>4. Cek Vreg3(3.3v) dan Vreg5(5v) atau nama lainnya LDO, pada ic DC/DC. Jika idak ada pada schema nama berikut cek tegangan pada schema yang bertuliskan 3.3v atau 5v dan buzzer pada tegangan tersebut.<br>5. Cek Enable (EN)'},
         { title: 'Hang / Stuck Logo BIOS', content: '1a. Coba Flash BIOS + BIOS EC jika ada, pastikan part yang terpasang sama dengan yang ada di skema.<br>1b. Clear ME Region BIOS.<br>2. Cuci Mesin.<br>3. Ganti Kapasitor 2.5v disekitar CPU jika masih menggunakan elco menjadi kapasitor SMD 330/470.<br>4. Disable VGA.' },
-        { title: 'Guide 3', content: 'This is the content for Guide 3.' }
+        { title: 'Disable RAM Onboard', content: '1 Cari DIMM,PCBID,BOARDID dan pindah semua Resistor ke 3V.' }
     ];
 
     // Filter and display results
